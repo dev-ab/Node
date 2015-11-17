aacff4a6e956bc2dcad19b9a395d8ab017f0aaa2
dev-ab <hellboyon@gmail.com>
2015-11-17 22:13:31 +0200

    initializing user story

---
+++ userstory/app/routes/api.js
@@ -0,0 +1,93 @@
+var User = require('../models/user');
+var config = require('../../config');
+var secretKey = config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
+
+function createToken(user) {
+    var token = jsonwebtoken.sign({
+        _id: user._id,
+        name: user.name,
+        username: user.username
+    }, secretKey, {
+        expiresInMinute: 1440
+    });
+
+    return token;
+}
+
+
+module.exports = function (app, express) {
+
+    var api = express.Router()
+    api.post('/signup', function (req, res) {
+        var user = new User({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        });
+        user.save(function (err) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json({message: 'User has been created!'});
+        });
+    });
+
+    api.get('/users', function (req, res) {
+        User.find({}, function (err, users) {
+            if (err) {
+                res.send(err);
+                return;
+            }
+            res.json(users);
+        });
+    });
+
+    api.post('/login', function (req, res) {
+        User.findOne({
+            username: req.body.username
+        }).select('password').exec(function (err, user) {
+            if (err)
+                throw err;
+
+            if (!user) {
+                res.send({message: "User doesn't exist"});
+            } else if (user) {
+                var validPassword = user.comparePassword(req.body.password);
+
+                if (!validPassword) {
+                    res.send({message: 'Invalid password'});
+                } else {
+                    var token = createToken(user);
+                    res.json({
+                        success: true,
+                        message: 'Successfuly logged in',
+                        token: token
+                    });
+                }
+            }
+        });
+    });
+
+    app.use(function (req, res, next) {
+        console.log('Someone just came to our app!');
+        var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+        if (token) {
+            jsonwebtoken.verify(token, secretKey, function (err, decoded) {
+                if (err) {
+                    res.status(403).send({success: flase, message: 'Failed to authenticate user'});
+                } else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            res.status(403).send({success: false, message: 'No token provided'});
+        }
+    });
+
+    return api;
+
+
+}
---
+++ userstory/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'database': 'mongodb://root:abc123@ds053194.mongolab.com:53194/users',
+    'port': process.env.PORT || 3000,
+    'secretKey': 'YourSecretKey'
+}
